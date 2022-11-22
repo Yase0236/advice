4560af1aabc2badae55c0068942d24017d3a2492
jasmin <yase0236@stud.kea.dk>
2022-11-22 13:11:43 +0100

    changes to fonts and color etc.

--- components/Calculate.jsx
+++ components/Calculate.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Calculate() {
   return (
     <>
-    <h3>Calculate your CO2 emission from your phone use here:</h3>
-    <p>Choose the social media you want to check and choose a time slot in each category. This calculator only applies to cell phones. 
-      In doubt about your used screen time? Check your phone’s settings to get the exact time spent on each app.</p>
-    <section className="time_section">
+      <h3>Calculate your CO2 emission from your phone use here:</h3>
+      <p>Choose the social media you want to check and choose a time slot in each category. This calculator only applies to cell phones. In doubt about your used screen time? Check your phone’s settings to get the exact time spent on each app.</p>
+      <section className="time_section">
         <label htmlFor="time-slot"></label>
-                <input required type="number" name="time-slot" id="time-slot" />
-    </section>
-    <section className="social_section">
+        <input required type="number" name="time-slot" id="time-slot" />
+      </section>
+      <section className="social_section">
         <label htmlFor="social-media"></label>
-                <input required type="text" name="social-media" id="social-media" />
-    </section>
+        <input required type="text" name="social-media" id="social-media" />
+      </section>
     </>
-  )
+  );
 }
