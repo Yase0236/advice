d67f751f8aed1579367da7de67c9ed925ab69f43
Sabine <sabi4826@stud.kea.dk>
2022-11-22 11:02:11 +0100

    Text added to Calculate

--- components/Calculate.jsx
+++ components/Calculate.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Calculate() {
   return (
     <>
-    <h3>Some headline here</h3>
-    <p>A lot of text here that explains the calculator, be clear and specific</p>
+    <h3>Calculate your CO2 emission from your phone use here:</h3>
+    <p>Choose the social media you want to check and choose a time slot in each category. This calculator only applies to cell phones. 
+      In doubt about your used screen time? Check your phone’s settings to get the exact time spent on each app.</p>
     <section className="time_section">
         <label htmlFor="time-slot"></label>
                 <input required type="number" name="time-slot" id="time-slot" />
