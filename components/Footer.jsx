fae6dfea0442b06aef9ff7b1f7182815de5b179b
jasmin <yase0236@stud.kea.dk>
2022-11-22 10:13:59 +0100

    changes to the footer and header

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>
-    <h2>Theme Assignment for Forte Advice - MMD KEA 2022 </h2>
-    <h3>Group members: Jasmin, Nancy and Sabine.</h3>
-  </div>;
+  return (
+    <div className="footer_styling">
+      <div className="footer_elements">
+        <h3>Theme Assignment for Forte Advice - MMD KEA 2022 </h3>
+        <h4>Group members: Jasmin, Nancy and Sabine.</h4>
+      </div>
+    </div>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>
-        <h2>Nice React(ion)</h2>  
-        <nav>
-            <ul>
-                <li>
+    <div className="header_styling">
+      <h2>Nice React(ion)</h2>
+      <nav>
+        <ul>
+          <li>
             <a href="#">About Us</a>
-            </li>
-            <li>
+          </li>
+          <li>
             <a href="#">Database</a>
-            </li>
-            <li>
+          </li>
+          <li>
             <a href="#">How we calculate</a>
-            </li>
-            <li>
+          </li>
+          <li>
             <a href="#">Contact</a>
-            </li>
-            </ul>
-        </nav>
-        </div>
-  )
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
