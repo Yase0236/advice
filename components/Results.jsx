194981d5d7ce34780790704c646471727eb2381d
Nancy <nanc0603@stud.kea.dk>
2022-11-23 12:54:08 +0100

    calculating the data in the calculate

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
-export default function Results() {
+import Calculate from "./Calculate";
+export default function Results(props) {
   return (
     <>
-      <h2>Here are your results</h2>
-      <p>Lorem</p>
+      <section className="result_hidden">
+        <h2>Here are your results</h2>
+
+        <p className="result_text"></p>
+      </section>
     </>
   );
 }
